a184ec54945816eb3191fff618a0253164c8221e
ShrinathNR <nr.shrinath@gmail.com>
2024-09-02 11:12:54 +0530

    update example counter.rs

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,30 +1,42 @@
-use anchor::prelude::*;
+use anchor_lang::prelude::*;
+declare_id!("HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1");
 #[program]
 pub mod VoteProgram {
-    declare_id!("HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1");
-    pub fn initialize(ctx: Context<InitializeContext>, hash: Vec<u8>) -> Result<()> {}
-    pub fn upvote(ctx: Context<UpvoteContext>, hash: Vec<u8>) -> Result<()> {}
-    pub fn downvote(ctx: Context<DownvoteContext>, hash: Vec<u8>) -> Result<()> {}
+    pub fn initialize(ctx: Context<InitializeContext>, hash: Vec<u8>) -> Result<()> {
+        ctx.state.vote = 0;
+        Ok(())
+    }
+    pub fn upvote(ctx: Context<UpvoteContext>, hash: Vec<u8>) -> Result<()> {
+        ctx.state.vote = ctx.accounts.state.vote + 1;
+        Ok(())
+    }
+    pub fn downvote(ctx: Context<DownvoteContext>, hash: Vec<u8>) -> Result<()> {
+        ctx.accounts.state.vote = ctx.accounts.state.vote - 1;
+        Ok(())
+    }
 }
 #[derive(Accounts)]
 pub struct InitializeContext<'info> {
-    # [account (init , payer = user , bump seeds = [b "vote" , hash])]
-    pub state: Account<'info, VoteState>,
-    #[account()]
+    #[account(mut)]
     pub user: Signer<'info>,
+    #[account(init, payer = user, space = 9, seeds = [b"vote", hash], bump)]
+    pub state: Account<'info, VoteState>,
     pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct UpvoteContext<'info> {
-    # [account (seeds = [b "vote" , hash])]
+    #[account(mut, seeds = [b"vote", hash], bump)]
     pub state: Account<'info, VoteState>,
+    pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct DownvoteContext<'info> {
-    # [account (seeds = [b "vote" , hash])]
+    #[account(mut, seeds = [b"vote", hash], bump)]
     pub state: Account<'info, VoteState>,
+    pub system_program: Program<'info, System>,
 }
 #[account]
 pub struct VoteState {
     pub vote: i64,
+    pub bump: u8,
 }
