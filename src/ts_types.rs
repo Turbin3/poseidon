c96bc06dfb269def8de04e112ec0f5527caad716
ShrinathNR <nr.shrinath@gmail.com>
2024-10-26 19:13:22 +0530

    add other array types

--- src/ts_types.rs
+++ src/ts_types.rs
@@ -21,7 +21,7 @@ pub const STANDARD_TYPES: [&str; 16] = [
     "Pubkey",
 ];
 
-pub const STANDARD_ARRAY_TYPES: [&str; 1] = ["Vec<String>"];
+pub const STANDARD_ARRAY_TYPES: [&str; 13] = ["Vec<String>", "Vec<u8>", "Vec<i8>", "Vec<u16>", "Vec<i16>", "Vec<u32>", "Vec<i32>", "Vec<u64>", "Vec<i64>", "Vec<u128>", "Vec<i128>", "Vec<Pubkey>", "Vec<boolean>"];
 
 pub const STANDARD_ACCOUNT_TYPES: [&str; 7] = [
     "Signer",
@@ -63,8 +63,20 @@ use crate::errors::PoseidonError;
 
 pub fn rs_type_from_str(str: &str) -> Result<TokenStream, Error> {
     match str {
-        "string" | "String" => Ok(quote! { String }),
-        "Vec<string>" | "Vec<String>" => Ok(quote! { Vec<String> }),
+        "String" => Ok(quote! { String }),
+        "Vec<String>" => Ok(quote! { Vec<String> }),
+        "Vec<u8>" => Ok(quote! { Vec<u8> }),
+        "Vec<i8>" => Ok(quote! { Vec<i8> }),
+        "Vec<u16>" => Ok(quote! { Vec<u16> }),
+        "Vec<i16>" => Ok(quote! { Vec<i16> }),
+        "Vec<u32>" => Ok(quote! { Vec<u32> }),
+        "Vec<i32>" => Ok(quote! { Vec<i32> }),
+        "Vec<u64>" => Ok(quote! { Vec<u64> }),
+        "Vec<i64>" => Ok(quote! { Vec<i64> }),
+        "Vec<u128>" => Ok(quote! { Vec<u128> }),
+        "Vec<i128>" => Ok(quote! { Vec<i128> }),
+        "Vec<Pubkey>" => Ok(quote! { Vec<Pubkey> }),
+        "Vec<boolean>" => Ok(quote! { Vec<bool> }),
         "u8" => Ok(quote! { u8 }),
         "i8" => Ok(quote! { i8 }),
         "u16" => Ok(quote! { u16 }),
